e00456b77478527127246617a6fb8a04ac2a2824
Timofey Zakovriashin <timurowtimur@gmail.com>
2025-10-18 20:36:01 +0300

    Создан базовый класс карточек товаров CardBase.ts

---
+++ src/components/View/cards/CardBase.ts
@@ -0,0 +1,52 @@
+import { IProduct } from "../../../types";
+import { Component } from "../../base/Component";
+import { IEvents } from "../../base/Events";
+import { CDN_URL } from "../../../utils/constants";
+import { setElementData } from "../../../utils/utils";
+
+export abstract class CardBase<T extends IProduct> extends Component<T> {
+  public data: T;
+  public events: IEvents;
+
+  constructor(container: HTMLElement, data: T, events: IEvents) {
+    super(container);
+    this.data = data;
+    this.events = events;
+  }
+  // IMG карточки товара
+  protected setImage(
+    image: HTMLImageElement,
+    src: string,
+    alt?: string | undefined
+  ) {
+    if (image) {
+      image.src = `${CDN_URL}/${src}`;
+      if (alt) {
+        image.alt = alt;
+      }
+    }
+  }
+  protected setPrice(price: HTMLSpanElement, value: number) {
+    if (price) {
+      price.textContent = value.toLocaleString();
+    }
+  }
+  protected setTitle(title: HTMLHeadingElement, value: string) {
+    if (title) {
+      title.textContent = value;
+    }
+  }
+
+  protected setCardData(element: HTMLElement) {
+    setElementData(element, {
+      id: this.data.id ?? "",
+      title: this.data.title,
+      description: this.data.description ?? "",
+      category: this.data.category ?? "",
+      price: this.data.price ?? "",
+      imageUrl: this.data.image ?? "",
+    });
+  }
+
+  public abstract render(): HTMLElement;
+}
